d64cbdd08c58f310b8c20c4d5c0d37cb427e6343
Kemal Ogün Işık <ogunisik@gmail.com>
2015-11-20 14:27:37 +0200

    problem9'un eklenmesi

--- projecteuler.py
+++ projecteuler.py
@@ -89,3 +89,15 @@ def problem8():
             maxValue = product
 
     return maxValue
+
+def problem9():
+    """ Special Pythagorean triplet
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+    """
+    for a in range(1, 1000 // 3):
+        for b in range(a, 1000 // 2):
+            c = 1000 - (a + b)
+            if a ** 2 + b ** 2 == c ** 2:
+                return a * b * c
